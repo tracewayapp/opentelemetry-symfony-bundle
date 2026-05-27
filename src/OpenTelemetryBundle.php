d41e71daa42c6efe1a6ee449364b9030bf59c8b6
Andreas Allacher <andreas.allacher@outlook.com>
2026-05-27 12:18:00 +0200

    [#22] some phpstan fixes.

--- src/OpenTelemetryBundle.php
+++ src/OpenTelemetryBundle.php
@@ -84,7 +84,7 @@ final class OpenTelemetryBundle extends Bundle
             return;
         }
 
-        /**@var array{enabled: bool, autoload_enabled: bool, use_putenv: bool, resource_attributes: array<string, string>, exporter_otlp_headers: array<string, string>} $sdkConfig */
+        /** @var array{enabled: bool, autoload_enabled: bool, use_putenv: bool, resource_attributes: array<string, string>, exporter_otlp_headers: array<string, string>} $sdkConfig */
         $sdkConfig = $this->container->getParameter('open_telemetry.sdk.config');
         $usePutEnv = $sdkConfig['use_putenv'];
 
